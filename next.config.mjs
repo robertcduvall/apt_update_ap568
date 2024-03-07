4ae5ca67494478848f6accd9f49b79f5f7e6fb22
Robert Duvall <rcd@cs.duke.edu>
2024-03-07 14:52:04 -0500

    Fixing silly error

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-/**
 const nextConfig = {
-  reactStrictMode: true,
+/**  reactStrictMode: true, */
   distDir: 'build',
+  output: 'export',
 };
-*/
 
 export default nextConfig;
